b4e79369eb8a4b4901c3942e0164f012641955eb
Hanseee16 <kamilfarhan223@gmail.com>
2023-08-27 23:58:26 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      // padding: "4rem",
+    },
+    fontFamily: {
+      poppins: ["poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
